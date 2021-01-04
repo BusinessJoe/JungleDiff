ea647a01d83609560ac43f18561753e85edb79c1
Lukas Boelling <lukasboelling002@gmail.com>
2021-01-04 12:39:25 -0500

    Create endpoint for POSTing a summoner name
    This endpoint will be used to request that the server downloads matches for the specified summoner.

--- backend/backend_server/jungle/tests.py
+++ backend/backend_server/jungle/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+
+class SummonerPostTestCase(TestCase):
+    def test_summoner_post_status_ok(self):
+        c = Client()
+        response = c.post('/api/summoner/', {'summoner_name': 'BusinessJoe'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_summoner_post_no_name_status_400(self):
+        c = Client()
+        response = c.post('/api/summoner/')
+        self.assertEqual(response.status_code, 400)
--- backend/backend_server/jungle/views.py
+++ backend/backend_server/jungle/views.py
@@ -1,10 +1,22 @@
 from dataclasses import asdict
 
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponseBadRequest, HttpResponseNotAllowed
 from riotwrapper.graph import Graph
 
 
-# Create your views here.
+def summoner_matches(request):
+    if request.method == 'POST':
+        if 'summoner_name' in request.POST:
+            summoner_name = request.POST['summoner_name']
+            print(summoner_name)
+            # Download match history
+            return HttpResponse()
+        else:
+            return HttpResponseBadRequest('Expected key `summoner_name`')
+    return HttpResponseNotAllowed(['POST'])
+
+
 def dragon_gold_diff(request, summoner_name):
     graph = Graph(0, 0, 10, 10, [(1, 1), (2, 2), (3, 1)])
     return JsonResponse(asdict(graph))
