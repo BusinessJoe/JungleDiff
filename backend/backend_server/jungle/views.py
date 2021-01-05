bd1181638fb1f485bbe568e971382b0d0684ed64
Lukas Boelling <lukasboelling002@gmail.com>
2021-01-04 22:41:46 -0500

    Refactor summoner_matches view

--- backend/backend_server/jungle/views.py
+++ backend/backend_server/jungle/views.py
@@ -5,8 +5,8 @@ from requests.exceptions import HTTPError
 
 from django.http import HttpResponse, JsonResponse
 from django.http import HttpResponseBadRequest, HttpResponseNotAllowed, HttpResponseNotFound
-from riotwrapper.graph import Graph
 
+from riotwrapper.graph import Graph
 from riotwrapper.models import Summoner, Match, Timeline
 from riotwrapper.api import LeagueApi
 
@@ -20,34 +20,37 @@ def summoner_matches(request):
         except KeyError:
             return HttpResponseBadRequest('Expected key `summoner_name`')
         else:
-            api = LeagueApi(os.environ['RIOT-API-TOKEN'], 'NA1')
             try:
-                summoner_dict = api.get_summoner_by_name(summoner_name)
+                save_summoner_match_history(summoner_name)
             except HTTPError as err:
-                if err.response.status_code == 404:
-                    return HttpResponseNotFound()
-                else:
-                    raise
-
-            summoner_dict['summonerId'] = summoner_dict['id']
-            del summoner_dict['id']
-
-            s = Summoner(**summoner_dict)
-            s.save()
-
-            match_list = api.get_matchlist_by_account_id(s.accountId, queue={420})
-            for match_summary in match_list['matches'][:5]:
-                game_id = match_summary['gameId']
-
-                match_dict = api.get_match_by_match_id(game_id)
-                match = Match(gameId=game_id, data=match_dict, summoner=s)
-                match.save()
-
-                timeline_dict = api.get_timeline_by_match_id(game_id)
-                timeline = Timeline(gameId=game_id, data=timeline_dict, summoner=s)
-                timeline.save()
+                # Replace requests HTTPError with equivalent Django HttpResponse
+                return HttpResponse(status=err.response.status_code)
             return HttpResponse()
     return HttpResponseNotAllowed(['POST'])
+
+
+def save_summoner_match_history(summoner_name):
+    """Save the summoner's most recent 5 ranked solo games to the database"""
+    api = LeagueApi(os.environ['RIOT-API-TOKEN'], 'NA1')
+    summoner_dict = api.get_summoner_by_name(summoner_name)
+
+    summoner_dict['summonerId'] = summoner_dict['id']
+    del summoner_dict['id']
+
+    s = Summoner(**summoner_dict)
+    s.save()
+
+    match_list = api.get_matchlist_by_account_id(s.accountId, queue={420})
+    for match_summary in match_list['matches'][:5]:
+        game_id = match_summary['gameId']
+
+        match_dict = api.get_match_by_match_id(game_id)
+        match = Match(gameId=game_id, data=match_dict, summoner=s)
+        match.save()
+
+        timeline_dict = api.get_timeline_by_match_id(game_id)
+        timeline = Timeline(gameId=game_id, data=timeline_dict, summoner=s)
+        timeline.save()
 
 
 def dragon_gold_diff(request, summoner_name):
