1373979b4bb09a7acf0ae46245031d0b61e40f71
Lukas Boelling <lukasboelling002@gmail.com>
2021-01-05 20:39:26 -0500

    Create footer for homepage

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,50 +1,57 @@
 import { React, Component } from 'react';
 import { Grid, Typography, TextField } from '@material-ui/core';
 import axios from 'axios';
+import './Home.css'
 
 axios.defaults.baseURL = 'http://localhost:8000';
 
 export default class Home extends Component {
-    constructor(props) {
-        super (props);
-    }
+  constructor(props) {
+    super (props);
+  }
 
-    enterPressed(e) {
-        var code = e.keyCode || e.which;
-        if(code === 13) {
-            axios.post("/api/summoner/", {
-					summoner_name: e.target.value,    
-				})
-				.then(response => {
-					console.log(response);
-					this.props.history.push("/test/" + e.target.value);
-				})
-				.catch(error => {
-					console.log(error);
-				}
-			);
-        }
+  enterPressed(e) {
+    var code = e.keyCode || e.which;
+    if(code === 13) {
+      axios.post("/api/summoner/", {
+        summoner_name: e.target.value,
+      })
+      .then(response => {
+        console.log(response);
+        this.props.history.push("/test/" + e.target.value);
+      })
+      .catch(error => {
+        console.log(error);
+      });
     }
+  }
 
-    render() {
-        return (
-            <div
-            style={{
-                position: 'absolute', left: '50%', top: '50%',
-                transform: 'translate(-50%, -50%)',
-            }}
-            >
-                <Grid container spacing={3} className="center" direction="row" align="center" justify="center">
-                    <Grid item xs={12} align="center">
-                        <Typography variant="h3" compact="h3">
-                            League API Lads
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} align="center">
-                        <TextField id="outlined-basic" label="Search Summoner" variant="outlined" fullWidth={true} onKeyPress={this.enterPressed.bind(this)}/>   
-                    </Grid>
-                </Grid>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div
+          style={{
+            position: 'absolute', left: '50%', top: '50%',
+            transform: 'translate(-50%, -50%)',
+          }}
+          >
+          <Grid container spacing={3} className="center" direction="row" align="center" justify="center">
+            <Grid item xs={12} align="center">
+              <Typography variant="h3" compact="h3">
+                League API Lads
+              </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+              <TextField id="outlined-basic" label="Search Summoner" variant="outlined" fullWidth={true} onKeyPress={this.enterPressed.bind(this)}/>
+            </Grid>
+          </Grid>
+        </div>
+        <footer>
+          <p>
+            Lorem ipsum
+          </p>
+        </footer>
+      </div>
+    );
+  }
 }
