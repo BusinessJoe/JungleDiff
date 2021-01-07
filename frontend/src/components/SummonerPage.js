5fe6b88febe5aebb54bbbbf25a2ec6b8ac455dd2
Lukas Boelling <lukasboelling002@gmail.com>
2021-01-07 01:02:41 -0500

    Query new graph endpoint for data

--- frontend/src/components/SummonerPage.js
+++ frontend/src/components/SummonerPage.js
@@ -2,22 +2,45 @@ import { React, Component } from 'react';
 import { Grid, Typography, TextField } from '@material-ui/core';
 import Navbar from './Navbar';
 import ChartTest from './ChartTest';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8000';
 
 export default class SummonerPage extends Component {
-    constructor(props) {
-        super (props);
-    }
+  constructor(props) {
+    super (props);
 
-    render() {
-        return (
-            <Grid container spacing ={0}>
-               <Grid item xs = {12}>
-                    <Navbar />
-                </Grid>
-                <Grid item xs = {12}>
-                    <ChartTest />
-                </Grid>
-            </Grid>
-        );
+    this.state = {
+      data: {
+        datasets: []
+      }
     }
+  }
+
+  componentDidMount() {
+    axios.get('api/summoner/JJamali/graph/dragon-gold-diff')
+    .then(({ data }) => {
+      this.setState({
+        data: {
+          datasets: [data]
+        }
+      });
+    })
+    .catch((err) => {});
+
+
+  }
+
+  render() {
+    return (
+      <Grid container spacing ={0}>
+        <Grid item xs = {12}>
+          <Navbar />
+        </Grid>
+        <Grid item xs = {12}>
+          <ChartTest data={this.state.data}/>
+        </Grid>
+      </Grid>
+    );
+  }
 }
