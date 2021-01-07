af042d0a0cb3666fa1ac6b566e60288f38cd3fb5
Lukas Boelling <lukasboelling002@gmail.com>
2021-01-07 00:45:15 -0500

    Add ability to get graph data for a summoner
    dragon_gold_diff.py allows easy calculation of the chances of securing
    first dragon based on botlane gold difference. The data is returned in
    a format usable by Chart.js.

--- backend/backend_server/jungle/tests.py
+++ backend/backend_server/jungle/tests.py
@@ -2,13 +2,14 @@ from django.test import Client
 from django.test import TestCase
 
 from riotwrapper.models import Summoner, Match, Timeline
+from jungle.stats import dragon_gold_diff
 
 
 class SummonerPostTestCase(TestCase):
     def test_summoner_post_status_ok(self):
         c = Client()
         response = c.post('/api/summoner/', {'summoner_name': 'BusinessJoe'})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 201)
 
     def test_summoner_post_no_name_status_400(self):
         c = Client()
@@ -34,3 +35,17 @@ class SummonerPostTestCase(TestCase):
         s = Summoner.objects.get(name='BusinessJoe')
         timelines = Timeline.objects.filter(summoner=s)
         self.assertTrue(timelines)
+
+
+class GraphsTestCase(TestCase):
+    def test_gold_dragon_diff_model(self):
+        from riotwrapper.api import LeagueApi
+        from jungle.views import save_summoner_match_history
+        import os
+        token = os.environ["RIOT-API-TOKEN"]
+        api = LeagueApi(token, "NA1")
+
+        summoner_name = "JJamali"
+        save_summoner_match_history(summoner_name, 5)
+        dataset = dragon_gold_diff.dataset_for_summoner(summoner_name, api)
+        self.assertTrue(dataset)
