0fa456e60ee8c5be1c138269cd6f8fd70fca35c5
Max Ning <maxning24@gmail.com>
2021-01-06 22:19:50 -0500

    Chart Test Added

--- frontend/src/components/SummonerPage.js
+++ frontend/src/components/SummonerPage.js
@@ -1,6 +1,7 @@
 import { React, Component } from 'react';
 import { Grid, Typography, TextField } from '@material-ui/core';
 import Navbar from './Navbar';
+import ChartTest from './ChartTest';
 
 export default class SummonerPage extends Component {
     constructor(props) {
@@ -12,7 +13,10 @@ export default class SummonerPage extends Component {
             <Grid container spacing ={0}>
                <Grid item xs = {12}>
                     <Navbar />
-                </Grid> 
+                </Grid>
+                <Grid item xs = {12}>
+                    <ChartTest />
+                </Grid>
             </Grid>
         );
     }
