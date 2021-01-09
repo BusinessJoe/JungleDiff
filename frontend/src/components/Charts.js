bd501d36e19a6161fb18b99812e7382d6ac75520
Lukas Boelling <lukasboelling002@gmail.com>
2021-01-08 19:24:37 -0500

    Add chart component

---
+++ frontend/src/components/Charts.js
@@ -0,0 +1,56 @@
+import { Component } from 'react';
+import { Scatter } from 'react-chartjs-2';
+import axios from 'axios';
+
+
+export default class DragonGoldDiffChart extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      summonerData: null,
+      comparisonData: null,
+    };
+  }
+
+  componentDidMount() {
+    // get summoner data
+    axios.get(`${this.props.Location}graph/dragon-gold-diff`)
+    .then(({ data }) => {
+      this.setState({
+        summonerData : data
+      });
+    })
+    .catch((err) => {console.log(err);});
+
+    // get comparison datasets
+    axios.get('/api/comparison/graph/dragon-gold-diff/')
+    .then(({ data }) => {
+      data.backgroundColor = '#AAAAAA';
+      this.setState({
+        comparisonData : data
+      });
+    })
+    .catch((err) => {console.log(err);});
+  }
+
+  render() {
+    var chartData = {};
+    if (this.state.summonerData && this.state.comparisonData) {
+      chartData = {
+        datasets:[this.state.summonerData, this.state.comparisonData]
+      }
+    }
+
+    const chartOptions = {
+      title: {
+        display: true,
+        text: 'Scatter',
+      },
+    }
+
+    return (
+      <Scatter data={chartData} options={chartOptions}/>
+    )
+  }
+}
