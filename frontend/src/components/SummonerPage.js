ccd0e511da89c10ed3e82e54d4becc6b7841dd73
Lukas Boelling <lukasboelling002@gmail.com>
2021-01-11 17:45:32 -0500

    Add graph explanation

--- frontend/src/components/SummonerPage.js
+++ frontend/src/components/SummonerPage.js
@@ -47,14 +47,29 @@ export default class SummonerPage extends Component {
               <Grid item xs={12} sm={6}>
                 <DragonGoldDiffChart Location={this.state.Location}/>
               </Grid>
-              <Grid item xs={12} sm={6}>
-                Graph explanation
+              <Grid item xs={12} sm={6} className='text-container'>
+                <p className='plaintext'>
+                  This graph represents your predicted chances of securing the
+                  first dragon based on data from your last 30 ranked games and
+                  compares it to the average diamond game.
+                </p>
+                <p className='plaintext'>
+                  For example, the average diamond player has a 50% chance of
+                  securing the first dragon if the botlane is perfectly even
+                  (0 gold difference). This chance grows to 75% if
+                  botlane is 300 gold ahead.
+                </p>
               </Grid>
             </Grid>
           </Paper>
         </div>
         <footer className='footer'>
-          <div className='footer-content'>[JungleDiff] © 2021 [JungleDiff] isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.
+          <div className='footer-content'>
+            [JungleDiff] © 2021 [JungleDiff] isn’t endorsed by Riot Games and
+            doesn’t reflect the views or opinions of Riot Games or anyone
+            officially involved in producing or managing League of Legends.
+            League of Legends and Riot Games are trademarks or registered
+            trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.
           </div>
         </footer>
       </div>
